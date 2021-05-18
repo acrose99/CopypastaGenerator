8f0a5e0fcf5577af5fa6104d87c84c6207b4841f
acrose99 <acrosemail@gmail.com>
2021-05-18 00:02:36 -0500

    woops

--- pages/GNU-Linux.tsx
+++ pages/GNU-Linux.tsx
@@ -17,21 +17,20 @@ export default function GNULinux() {
     const [partString, setPartString] = useState("The kernel is an essential part of an operating system, but useless by itself; it can only function in the context of a complete operating system");
 
 
-    const linuxString = "I'd just like to interject for a moment.  What you're referring to as " + {referString} + ",\n" +
-        "is in fact, " + {actuallyString} + "/" + {referString} + ", or as I've recently taken to calling it, " + {actuallyString} + " plus " + {referString} + ".\n" +
-        "" + {referString} + " is not an operating system unto itself, but rather another free component\n" +
-        "of a fully functioning " + {actuallyString} + " system made useful by the " + {actuallyString} + " corelibs, shell\n" +
-        " " + {definedByString} +
-        "Many " + {userString} + "run a modified version of the " + {actuallyString} + " system every day,\n" +
-        "without realizing it.  Through a peculiar turn of events, the version of " + {actuallyString} + "\n" +
-        "which is widely used today is often called \"" + {referString} + "\", and many of its users are\n" +
-        "not aware that it is basically the " + {actuallyString} + " system, developed by the " + {actuallyString} + " Project." +
-
-        "There really is a " + {referString} + ", and these people are using it, but it is just a\n" +
-        "part of the system they use.  " + {referString} + " " + {kernelString} + + " " + {partString} + " " + referString +
-        " is normally used in combination with the " + {actuallyString} + " operating system: the whole system\n" +
-        "is basically " + {actuallyString} + " with " + {referString} + " added, or " + {actuallyString} + "/" + {referString} + ".  All the so-called \"" + {referString} + "\"\n" +
-        "distributions are really distributions of " + {actuallyString} + "/" + {referString} + "."
+    const linuxString = "I'd just like to interject for a moment.  What you're referring to as " + referString +
+        " is in fact, " + actuallyString + "/" + referString + ", or as I've recently taken to calling it, " + actuallyString + " plus " + referString + ". " + referString
+          + " is not an operating system unto itself, but rather another free component " +
+        "of a fully functioning " + actuallyString + " system made useful by the " + actuallyString + " corelibs, shell" +
+        " " + definedByString +
+        ". Many " + userString + " run a modified version of the " + actuallyString + " system every day," +
+        "without realizing it. Through a peculiar turn of events, the version of " + actuallyString +
+        " which is widely used today is often called " + referString + ", and many of its users are " +
+        "not aware that it is basically the " + actuallyString + " system, developed by the " + actuallyString + " Project." +
+        " There really is a " + referString + ", and these people are using it, but it is just a" +
+        "part of the system they use. " + referString + " " + kernelString + ". " + partString + " " + referString +
+        " is normally used in combination with the " + actuallyString + " operating system: the whole system " +
+        "is basically " + actuallyString + " with " + referString + " added, or " + actuallyString + "/" + referString + ". All the so-called " + referString +
+        " distributions are really distributions of " + actuallyString + "/" + referString + "."
 
     function handleChange(functionCalled, e) {
         functionCalled(e.target.value);
@@ -40,7 +39,7 @@ export default function GNULinux() {
     return (
         <Layout>
             <Head>
-                <title>Rick and Morty Copypasta</title>
+                <title>GNU/Linux Copypasta</title>
             </Head>
             <header>
                 <h1 className={styles.header}>GNU/Linux Copypasta</h1>
@@ -88,14 +87,14 @@ export default function GNULinux() {
 
                     Many  <span className={styles.span}> {userString}</span> run a modified version of the <span className={styles.span}> {actuallyString} </span> system every day,
                     without realizing it.  Through a peculiar turn of events, the version of <span className={styles.span}> {actuallyString} </span>
-                    which is widely used today is often called "<span className={styles.span}> {referString} </span>", and many of its users are
+                    which is widely used today is often called "<span className={styles.span}>{referString}</span>", and many of its users are
                     not aware that it is basically the <span className={styles.span}> {actuallyString} </span> system, developed by the <span className={styles.span}> {actuallyString} </span> Project.
 
                     There really is a <span className={styles.span}> {referString} </span>, and these people are using it, but it is just a
                     part of the system they use.  <span className={styles.span}> {referString} </span> is the <span className={styles.span}>{kernelString}</span>.
                     The <span className={styles.span}> {partString} </span> <span className={styles.span}> {referString} </span> is
                     normally used in combination with the <span className={styles.span}> {actuallyString} </span> operating system: the whole system
-                    is basically <span className={styles.span}> {actuallyString} </span> with <span className={styles.span}> {referString} </span> added, or <span className={styles.span}> {actuallyString} </span>/<span className={styles.span}> {referString} </span>.  All the so-called "<span className={styles.span}> {referString} </span>"
+                    is basically <span className={styles.span}> {actuallyString} </span> with <span className={styles.span}> {referString} </span> added, or <span className={styles.span}> {actuallyString} </span>/<span className={styles.span}> {referString} </span>. All the so-called "<span className={styles.span}> {referString} </span>"
                     distributions are really distributions of <span className={styles.span}> {actuallyString} </span>/<span className={styles.span}> {referString} </span>.
                 </p>
                 <CopyToClipboard onCopy={() => setCopied(true)} text={linuxString}>
